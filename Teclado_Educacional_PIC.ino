ef36376576aecc1944cb7701696f1a6fcf4e826e
paisdegales <76596868+paisdegales@users.noreply.github.com>
2021-05-11 15:17:58 -0300

    Update Teclado_Educacional_PIC.ino

--- Teclado_Educacional_PIC.ino
+++ Teclado_Educacional_PIC.ino
@@ -8,10 +8,11 @@ void freeStyle(int tom[]);
 //Função para o modo aprendizado
 void musica();
 
-//
-int i = 0;
+// Vetor com o valor das portas do arduino referentes as teclas de C(Dó) a B(Si)
 int keys[] = {14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+// Vetor com o valor das portas do arduino referentes aos leds de C(Dó) a B(Si)
 int leds[] = {38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49};
+// Vetor das oitavas de um piano (oitava: conjunto de 7 notas e 5 acidentes (sustenidos))
 int tones[][13] = {  {NOTE_C1, NOTE_CS1 , NOTE_D1, NOTE_DS1, NOTE_E1, NOTE_F1, NOTE_FS1, NOTE_G1, NOTE_GS1, NOTE_A1, NOTE_AS1, NOTE_B1}
                     ,{NOTE_C2, NOTE_CS2 , NOTE_D2, NOTE_DS2, NOTE_E2, NOTE_F2, NOTE_FS2, NOTE_G2, NOTE_GS2, NOTE_A2, NOTE_AS2, NOTE_B2}
                     ,{NOTE_C3, NOTE_CS3 , NOTE_D3, NOTE_DS3, NOTE_E3, NOTE_F3, NOTE_FS3, NOTE_G3, NOTE_GS3, NOTE_A3, NOTE_AS3, NOTE_B3}
@@ -21,18 +22,22 @@ int tones[][13] = {  {NOTE_C1, NOTE_CS1 , NOTE_D1, NOTE_DS1, NOTE_E1, NOTE_F1, N
                     ,{NOTE_C7, NOTE_CS7 , NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7}}; // melhor tom
 
 
+//Vetor com a sequência das teclas a serem tocadas no modo aprendizado
 int keys_musica[sizeof(ordem)/sizeof(int)];
+//Vetor das notas em sequência com seus respectivos tons para o modo aprendizado
 int musicas[sizeof(ordem)/sizeof(int)];
+//Vetor com a sequencia dos leds a acenderem no modo aprendizado
 int leds_musica[sizeof(ordem)/sizeof(int)];
 
 void setup() {
-  
+  //Preenchimento dos vetores listados fora do setup (modo aprendizado)
   for(int i = 0; i < (sizeof(ordem)/sizeof(int)); i++){
     keys_musica [i] = keys[ordem[i]];
     musicas[i] = tones[tom[i]-1][ordem[i]];
     leds_musica[i] = leds[ordem[i]];
   }
   
+  //SPK: macro que contem a porta do arduino referente ao speaker (definido na biblioteca)
   pinMode(SPK, OUTPUT);
   
   for(int i = 0; i < 12; i++){
@@ -68,7 +73,7 @@ void freeStyle(int tom[]){
   }
 }
 void musica(){
-  int lastState = LOW;
+  int lastState = LOW, i=0;
   while(digitalRead(keys_musica[i])){
        tone(SPK, musicas[i]);
        digitalWrite(leds_musica[i], LOW);
